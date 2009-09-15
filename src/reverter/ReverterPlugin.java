9471a9295e021049494404c141b47d5204adb635
upliner <upliner@7c512e10-a15e-11de-bc59-9baca26b92a8>
2009-09-15 18:49:16 +0000

    initial development of plugin
    git-svn-id: http://svn.nbk/repos/josm-plugins/reverter@6 7c512e10-a15e-11de-bc59-9baca26b92a8

--- src/reverter/ReverterPlugin.java
+++ src/reverter/ReverterPlugin.java
@@ -1,9 +1,24 @@
 package reverter;
 
+import static org.openstreetmap.josm.tools.I18n.tr;
+import javax.swing.JMenu;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
+
 
 public class ReverterPlugin extends Plugin {
 	public ReverterPlugin()
 	{
+		JMenu historyMenu = new JMenu(tr("History"));
+		Main.main.menu.add(historyMenu);
+		MainMenu.add(historyMenu, new ReverterAction());	   
+
 	}
+    public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame)
+    {
+    }
+	
 }
